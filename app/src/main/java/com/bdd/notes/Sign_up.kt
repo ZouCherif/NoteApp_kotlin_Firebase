c6b1b938b40b36c1896807b63004cdea1b3946cc
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-10 22:41:56 +0100

    Signup activity changes

--- app/src/main/java/com/bdd/notes/Sign_up.kt
+++ app/src/main/java/com/bdd/notes/Sign_up.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.bdd.notes.databinding.ActivitySignUpBinding
 
 class Sign_up : AppCompatActivity() {
-
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-        val btn1: Button = findViewById(R.id.SignIn)
-        btn1.setOnClickListener {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toSignIn.setOnClickListener {
             val intent = Intent (this,Sign_in::class.java)
             startActivity(intent)
         }
