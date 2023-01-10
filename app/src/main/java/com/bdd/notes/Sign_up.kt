8644c4a4ba0032860c02579a127c7a265bce6d88
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-10 22:30:31 +0100

    Enable viewBinding

--- app/src/main/java/com/bdd/notes/Sign_up.kt
+++ app/src/main/java/com/bdd/notes/Sign_up.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Sign_up : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
