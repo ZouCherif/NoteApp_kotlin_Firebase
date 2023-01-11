b244c03af33f5f198fc31bf4f2e26b6b93241190
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-11 23:05:40 +0100

    adding notes to the database

--- app/src/main/java/com/bdd/notes/Add_note.kt
+++ app/src/main/java/com/bdd/notes/Add_note.kt
@@ -2,10 +2,41 @@ package com.bdd.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bdd.notes.databinding.ActivityAddNoteBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.time.LocalDateTime
 
 class Add_note : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private  lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.savebtn.setOnClickListener{
+            val title = binding.title.text.toString()
+            val body = binding.body.text.toString()
+            val time = LocalDateTime.now()
+
+            if (title.isNotEmpty() && body.isNotEmpty()){
+                addNoteToDB(title, body, time, FirebaseAuth.getInstance().currentUser?.uid)
+            }
+        }
+    }
+
+    private fun addNoteToDB(title: String?, body: String?, time: LocalDateTime, uid: String?){
+        mDbRef = FirebaseDatabase.getInstance().getReference("users")
+        val key = mDbRef.child(uid!!).child("notes").push().key
+        val note = Note(title, body, time)
+        mDbRef.child(uid!!).child("notes").child(key!!).setValue(note).addOnSuccessListener {
+            finish()
+        }.addOnFailureListener {
+            Toast.makeText(this, "error", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
--- app/src/main/java/com/bdd/notes/Note.kt
+++ app/src/main/java/com/bdd/notes/Note.kt
@@ -1,10 +1,18 @@
 package com.bdd.notes
 
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
-import java.util.Date
+import java.time.LocalDateTime
 
 @IgnoreExtraProperties
-data class Note(val title: String? = null, val body: String? = null, val time: Date) {
-
+data class Note(val title: String? = null, val body: String? = null, val time: LocalDateTime) {
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "title" to title,
+            "body" to body,
+            "time" to time,
+        )
+    }
 }
