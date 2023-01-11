c045710cf4ba193dde6cb9e9c4378d9b2561f436
Oizys13 <brchred@gmail.com>
2023-01-12 00:32:39 +0100

    main screen updated

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/bdd/notes/databinding/ActivityAddNoteBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/bdd/notes/databinding/ActivityAddNoteBinding.java
@@ -4,20 +4,37 @@ package com.bdd.notes.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.bdd.notes.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityAddNoteBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityAddNoteBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final EditText body;
+
+  @NonNull
+  public final AppCompatButton savebtn;
+
+  @NonNull
+  public final EditText title;
+
+  private ActivityAddNoteBinding(@NonNull ConstraintLayout rootView, @NonNull EditText body,
+      @NonNull AppCompatButton savebtn, @NonNull EditText title) {
     this.rootView = rootView;
+    this.body = body;
+    this.savebtn = savebtn;
+    this.title = title;
   }
 
   @Override
@@ -43,10 +60,31 @@ public final class ActivityAddNoteBinding implements ViewBinding {
 
   @NonNull
   public static ActivityAddNoteBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.body;
+      EditText body = ViewBindings.findChildViewById(rootView, id);
+      if (body == null) {
+        break missingId;
+      }
 
-    return new ActivityAddNoteBinding((ConstraintLayout) rootView);
+      id = R.id.savebtn;
+      AppCompatButton savebtn = ViewBindings.findChildViewById(rootView, id);
+      if (savebtn == null) {
+        break missingId;
+      }
+
+      id = R.id.title;
+      EditText title = ViewBindings.findChildViewById(rootView, id);
+      if (title == null) {
+        break missingId;
+      }
+
+      return new ActivityAddNoteBinding((ConstraintLayout) rootView, body, savebtn, title);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
