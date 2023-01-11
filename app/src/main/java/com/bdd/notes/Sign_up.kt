4bd1ee151dfb8a857f608796d7745091803e10e1
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-11 20:57:42 +0100

    adding users to the DataBase

--- app/src/main/java/com/bdd/notes/Sign_up.kt
+++ app/src/main/java/com/bdd/notes/Sign_up.kt
@@ -6,10 +6,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bdd.notes.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class Sign_up : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private  lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -32,7 +37,7 @@ class Sign_up : AppCompatActivity() {
                 firebaseAuth.createUserWithEmailAndPassword(email, pass)
                     .addOnSuccessListener {
                         firebaseAuth.currentUser?.sendEmailVerification()
-                        //addUserToDB(username, email, firebaseAuth.currentUser?.uid!!)
+                        addUserToDB(username, email, firebaseAuth.currentUser?.uid!!)
                         val intent = Intent(this, Sign_in::class.java)
                         startActivity(intent)
                         finish()
@@ -42,11 +47,28 @@ class Sign_up : AppCompatActivity() {
                             .show()
                     }
             }else{
-                Toast.makeText(this, "empty fields are not allowed !!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Empty fields are not allowed !!", Toast.LENGTH_LONG).show()
             }
         }
     }
 
+    private fun addUserToDB(username: String, email: String, uid: String){
+        mDbRef = FirebaseDatabase.getInstance().getReference("users")
+        val user = User(username, email)
+        mDbRef.child(uid).setValue(user).addOnSuccessListener {
+            Toast.makeText(this, "Please Chek your email", Toast.LENGTH_LONG).show()
+            Firebase.auth.signOut()
+        }
+            .addOnFailureListener {
+                Toast.makeText(this, "error", Toast.LENGTH_LONG).show()
+            }
+
+
+        //to solve the probleme of adding users to DB
+        //Download an updated google-services.json and use that in your app, or
+        //Provide the database URL in your code with FirebaseDatabase database = FirebaseDatabase.getInstance("database URL here");
+
+    }
 
 
 }
---
+++ app/src/main/java/com/bdd/notes/User.kt
@@ -0,0 +1,8 @@
+package com.bdd.notes
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User(val username: String? = null, val email: String? = null) {
+
+}
