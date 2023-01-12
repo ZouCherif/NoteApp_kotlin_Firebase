2270c9b54dd7cc6a6ae2f78ddadbf3768eaf9bb5
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-13 00:26:30 +0100

    modifying notes Done

--- app/src/main/java/com/bdd/notes/modifyNote.kt
+++ app/src/main/java/com/bdd/notes/modifyNote.kt
@@ -2,11 +2,18 @@ package com.bdd.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bdd.notes.databinding.ActivityMainScreenBinding
 import com.bdd.notes.databinding.ActivityModifyNoteBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class modifyNote : AppCompatActivity() {
     private lateinit var binding: ActivityModifyNoteBinding
+    private  lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityModifyNoteBinding.inflate(layoutInflater)
@@ -18,5 +25,24 @@ class modifyNote : AppCompatActivity() {
 
         binding.body.setText(body)
         binding.title.setText(title)
+
+        binding.modify.setOnClickListener {
+            mDbRef = FirebaseDatabase.getInstance().getReference("users")
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+            val time = LocalDateTime.now().format(formatter)
+            val note = Note(binding.title.text.toString(),
+                    binding.body.text.toString(),
+                    time,
+                    noteid)
+            mDbRef.child(FirebaseAuth.getInstance().currentUser?.uid!!).child("notes")
+                .child(noteid!!).setValue(note).addOnSuccessListener {
+                    Toast.makeText(this, "note successfully updated", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+        }
+
+        binding.delete.setOnClickListener{
+            
+        }
     }
 }
