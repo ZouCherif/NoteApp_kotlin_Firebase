8aea846e320301befd17f679329fdac5fe3155a1
Oizys13 <brchred@gmail.com>
2023-01-13 00:43:42 +0100

    modify note

--- app/src/main/java/com/bdd/notes/NotesAdapter.kt
+++ app/src/main/java/com/bdd/notes/NotesAdapter.kt
@@ -1,6 +1,7 @@
 package com.bdd.notes
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,6 +22,10 @@ class NotesAdapter(val context: Context, val noteList: ArrayList<Note>): Recycle
         val currentNote = noteList[position]
         holder.title.text = currentNote.title
         holder.time.text = currentNote.time
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, modifyNote::class.java)
+            intent.putExtra("")
+        }
     }
 
     class NoteViewholder(itemView: View): RecyclerView.ViewHolder(itemView){
